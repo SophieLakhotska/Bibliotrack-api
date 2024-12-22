33e158f7764eee8c69a177ed51e6981f3e1c0d6b
SophieLakhotska <146372296+SophieLakhotska@users.noreply.github.com>
2024-12-22 12:12:23 +0200

    Add files via upload

---
+++ src/main/java/com/bibliotrack/bibliotrackapi/BibliotrackApiApplication.java
@@ -0,0 +1,12 @@
+package com.bibliotrack.bibliotrackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotrackApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BibliotrackApiApplication.class, args);
+  }
+}
